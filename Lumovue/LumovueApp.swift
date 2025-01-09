078cca956daec1c77faba4c9bd6703a0b47e10c6
Rohit <rohit.sharma9934@gmail.com>
2025-01-09 08:47:47 +0530

    Initial Commit

---
+++ Lumovue/LumovueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumovueApp.swift
+//  Lumovue
+//
+//  Created by Rohit on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LumovueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
