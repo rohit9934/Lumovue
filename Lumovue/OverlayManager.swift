90bce31375d5e39f26dde2cea79d0faed1086326
Rohit <rohit.sharma9934@gmail.com>
2025-01-12 20:11:03 +0530

    Getting the scale in animation right
    Added Code for the Scale in animation

--- Lumovue/OverlayManager.swift
+++ Lumovue/OverlayManager.swift
@@ -13,26 +13,37 @@ class OverlayManager: ObservableObject {
 	
 	func showOverlay() {
 		let screens = NSScreen.screens
-		
-		// Create an overlay window for each screen
+
 		overlayWindows = screens.map { screen -> NSWindow in
+			let initialSize = NSSize(width: screen.frame.width * 0.8, height: screen.frame.height * 0.8) // Small size to start
+			
 			let overlayWindow = NSWindow(
-				contentRect: screen.frame,
+				contentRect: NSRect(x: screen.frame.origin.x + (screen.frame.width - initialSize.width) / 2,
+									y: screen.frame.origin.y + (screen.frame.height - initialSize.height) / 2,
+									width: initialSize.width,
+									height: initialSize.height),
 				styleMask: [.borderless],
 				backing: .buffered,
 				defer: false
 			)
-			overlayWindow.level = .mainMenu + 1  // Above all other windows
-			overlayWindow.backgroundColor = NSColor.black
+			overlayWindow.level = .mainMenu + 1
+			overlayWindow.backgroundColor = NSColor.black.withAlphaComponent(0)
 			overlayWindow.makeKeyAndOrderFront(nil)
-			overlayWindow.isOpaque = true
+			overlayWindow.isOpaque = false
 			overlayWindow.ignoresMouseEvents = false
 			overlayWindow.collectionBehavior = [.stationary, .ignoresCycle]
 			
-			// Set up the overlay view with a dismiss button
 			let hostingView = NSHostingView(rootView: OverlayView(manager: self))
 			overlayWindow.contentView = hostingView
 			
+			// Animate scaling and opacity
+			NSAnimationContext.runAnimationGroup { context in
+				context.duration = 0.5
+				context.allowsImplicitAnimation = true
+				overlayWindow.animator().setFrame(screen.frame, display: true)
+				overlayWindow.animator().backgroundColor = NSColor.black.withAlphaComponent(1)
+			}
+
 			return overlayWindow
 		}
 	}
